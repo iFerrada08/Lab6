d2c8a2a11137525504608803fd7c44f5c6ff174b
Omar Aviles <oaviles@live.com>
2022-11-26 08:14:36 -0600

    Update assign-policy.sh

--- policy/assign-policy.sh
+++ policy/assign-policy.sh
@@ -1,4 +1,4 @@
 
 az login --service-principal -u $1 -p $2 --tenant $3
 
-az policy assignment create --policy {Audit Tags for All Services} -p "{ \"Tag Name\": { \"value\": [ \"Env\" ] }, \"Tag Value\": { \"value\": [ \"Dev\" ] } }"
+az policy assignment create --policy audit-tag-x-all-services -p "{ \"Tag Name\": { \"value\": [ \"Env\" ] }, \"Tag Value\": { \"value\": [ \"Dev\" ] } }"
