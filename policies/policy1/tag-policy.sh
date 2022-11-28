b52aa7df68f0efc1350013514c386f1f4552a2ee
Omar Aviles <oaviles@live.com>
2022-11-28 09:05:20 -0600

    Update tag-policy.sh

--- policies/policy1/tag-policy.sh
+++ policies/policy1/tag-policy.sh
@@ -1,4 +1,4 @@
 
 az login --service-principal -u $1 -p $2 --tenant $3
 
-az policy definition create --name audit-tag-x-all-services --display-name 'Audit Tags for All Services' --description 'This policy ensures that all azure services use a tag' --rules './policies/policy_definition/azurepolicy.rules.json' --params './policies/policy_definition/azurepolicy.parameters.json' --mode All
+az policy definition create --name audit-tag-x-all-services --display-name 'Audit Tags for All Services' --description 'This policy ensures that all azure services use a tag' --rules './policies/policy1/policy_definition/azurepolicy.rules.json' --params './policies/policy1/policy_definition/azurepolicy.parameters.json' --mode All
